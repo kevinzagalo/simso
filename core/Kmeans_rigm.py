c5e4756b06c2bb2e23b87b8de0b70f230455ad92
MarcAntoineAuvray <marc-antoine.auvray.auditeur@lecnam.net>
2022-10-10 17:43:41 +0200

    Kmeans_rigm.py

--- core/Kmeans_rigm.py
+++ core/Kmeans_rigm.py
@@ -12,8 +12,8 @@ from simso.core.results import Results
 from simso.estimation.Modes import Modes
 from simso.estimation.Kmeans_inertia import *
 
-from simso.estimation.rInverseGaussian.rInvGaussMixture import rInvGaussMixture
-
+from rInverseGaussian.rInvGaussMixture import rInvGaussMixture
+#from simso.estimation.rInverseGaussian.rInvGaussMixture import rInvGaussMixture
 import matplotlib.pyplot as plt
 from numpy import array
 import copy
@@ -24,94 +24,72 @@ class Kmeans_rigm:
     def __init__(self,
                  alpha=0.1):
         self.alpha=alpha
-        self.model=None
+        self.model={}
         self.n_components_rigm_max = 6
-
 
     def delete_0(self, X):
         return array( [list_tasks for list_tasks in X if 0 not in list_tasks] )
 
-
-    def write_json_files(self, XX):
+    def fit(self, XX):
 
         kmeans_inertia_ = Kmeans_inertia(alpha=self.alpha)
         XX = kmeans_inertia_.delete_0(array(XX))
-
         kmeans_inertia_.fit(X=XX)
 
-        centroids = kmeans_inertia_.get_centroids()
-        # centroids n column = task n
-        # centroids n row = class n
-
-        dict_response_times_by_task = kmeans_inertia_.dict_response_times_by_task(X=XX)
-        # dict = { task_n : { class_n : values ,...},...}
-
-        dict_by_tasks_centroids = kmeans_inertia_.dict_response_times_by_task(X=centroids)
-        # dict = { task_n : { class_n : centroid of values ,...},...}
-
-        for task_n in dict_response_times_by_task :
-            for class_n in dict_response_times_by_task[task_n]:
-                dict_response_times_by_task[task_n][class_n] = {"values":dict_response_times_by_task[task_n][class_n],
-                                                                "centroid":dict_by_tasks_centroids[task_n][class_n][0]}
-
-        dict_params = copy.deepcopy(dict_response_times_by_task)
-
+        list_predicted_classes = kmeans_inertia_.predict(XX)
+        list_response_times_by_task = [list(list_response_times_by_task) for list_response_times_by_task in list(numpy.transpose(XX))]
         list_n_components = list(range(1, self.n_components_rigm_max))
 
+        dict_kmeans_rigm_params = dict.fromkeys(list(range(kmeans_inertia_.n_clusters)))
 
-        for task_n in dict_response_times_by_task:
+        for unique_class in dict_kmeans_rigm_params:
 
-            for class_n in dict_response_times_by_task[task_n]:
+            dict_kmeans_rigm_params[unique_class] = dict.fromkeys(list(range(len(list_response_times_by_task))))
+            for task_n in dict_kmeans_rigm_params[unique_class]:
 
-                task_ = dict_response_times_by_task[task_n][class_n]["values"]
-                list_bics = []
+                list_tasks_class_n_task_n = [list_response_times_by_task[task_n][index] for index in range(len(list_predicted_classes)) if list_predicted_classes[index]==unique_class]
+
+                list_bics_class_n_task_n=[]
                 for n_components in list_n_components:
-                    r_inv_gauss = rInvGaussMixture(n_components=n_components)
-                    r_inv_gauss.fit(X=task_)
-                    list_bics.append(r_inv_gauss.bic(X=task_))
+                    r_inv_gauss_mixture_test = rInvGaussMixture(n_components=n_components)
+                    r_inv_gauss_mixture_test.fit(X=list_tasks_class_n_task_n)
+                    list_bics_class_n_task_n.append(r_inv_gauss_mixture_test.bic(X=list_tasks_class_n_task_n))
 
-                best_n_bytask_byclass = list_n_components[list_bics.index(max(list_bics))]
+                r_inv_gauss_mixture = rInvGaussMixture(n_components=list_n_components[list_bics_class_n_task_n.index(max(list_bics_class_n_task_n))])
+                r_inv_gauss_mixture.fit(X=list_tasks_class_n_task_n)
+                dict_kmeans_rigm_params[unique_class][task_n]=r_inv_gauss_mixture.get_parameters()
 
-                r_inv_gauss = rInvGaussMixture(n_components=best_n_bytask_byclass)
-                r_inv_gauss.fit(X=task_)
-                dict_params[task_n][class_n].update(r_inv_gauss.get_parameters())
+            dict_kmeans_rigm_params[unique_class]["centroids"]=list(kmeans_inertia_.get_centroids()[unique_class])
 
-                del dict_params[task_n][class_n]['values']
+        self.model = dict_kmeans_rigm_params
 
-        with open("./simso/core/parameters.json", "w") as outfile:
-            json.dump(dict_params, outfile)
+    def predict(self, list_lists_tasks):
 
-    def get_parameters(self):
-        self.parameters = json.load(open("./simso/core/parameters.json"))
-
-        return self.parameters
+        centroids = [ self.model[class_n]["centroids"] for class_n in self.model ]
 
 
-    def fit(self):
+        kmeans = KMeans(n_clusters=len(centroids), n_init=1, random_state=1)
 
-        parameters = self.get_parameters()
-        # dict paramaters = { task_0 : { kmeans_class_0 : {centroid: ., weights: .,modes: .,cv: .,n_components: .},...},...}
+        # pas fini
 
-        centroids = []
-        for task_n in parameters:
+        if type(list_lists_tasks[0])==int:
+            list_lists_tasks=[list_lists_tasks] # [,,,]=>[[,,,]]
 
-            task_n_class_n_centroids=[]
-            for class_n in parameters[task_n]:
-                task_n_class_n_centroids.append(parameters[task_n][class_n]["centroid"])
+        class_predicted = self.kmeans.predict(list_lists_tasks)
 
-            centroids.append(task_n_class_n_centroids)
+        for tasks_i in range(len(list_lists_tasks)):
 
-        self.kmeans = KMeans(n_clusters=len(centroids), n_init=1, random_state=1)
+            for task_n in range(len(list_lists_tasks[tasks_i])):
 
-        self.kmeans.fit(centroids)
+                response_time=list_lists_tasks[tasks_i][task_n]
 
-        return self.kmeans
+                rigm_parameters = self.get_rigm_parameters()[str(task_n)][str(class_predicted[tasks_i])]
 
+                r_inv_gauss_mixture = rInvGaussMixture(n_components=rigm_parameters["n_components"])
+                r_inv_gauss_mixture.weights_ = rigm_parameters["weights"]
+                r_inv_gauss_mixture.modes_ = rigm_parameters["modes"]
+                r_inv_gauss_mixture.cv_ = rigm_parameters["cv"]
 
-    def predict(self, tasks):
+                print(response_time)
+                print(r_inv_gauss_mixture.predict([response_time]))
 
-        parameters = self.get_parameters()
-        class_predicted = self.kmeans.predict(tasks)[0]
-
-        for i in list(range(0, len(tasks[0] ))):
-            print(parameters[str(i)][str(class_predicted)])
